29476fbce9340abaff1e1b07d59db549ab5dacab
illogicalloss <illogicalloss@gmail.com>
2022-08-29 18:28:52 +0000

    Added test folder and package dir

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -1,5 +1,4 @@
 import unittest
-
 from translator import english_to_french, french_to_english
 
 class TestTranslator(unittest.TestCase):
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,27 +1,31 @@
-import json
+"""This module is for converting between English and French"""
+import os
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-apikey = os.environ['apikey']
-url = os.environ['url']
+#apikey = os.environ['apikey']
+#url = os.environ['url']
 
-authenticator = IAMAuthenticator(apikey)
+authenticator = IAMAuthenticator('tgDj32PWjadzuQdqNRXZQeemoELi7uWxSnc-othwoeVC')
 language_translator = LanguageTranslatorV3(\
 version='2018-05-01'\
 ,authenticator=authenticator)
 
-language_translator.set_service_url(url)
+language_translator.set_service_url('https://api.us-south.language-translator.watson.cloud.ibm.com/instances/443227b6-f49d-4890-81b4-c9295831b64a')
 
-def englishToFrench(englishText):
-    french = language_translator.translate(text=englishText,model_id='en-fr').get_result()
+def english_to_french(english_text):
+    """
+    Translates an english word or phrase to French
+    """
+    french = language_translator.translate(text=english_text,model_id='en-fr').get_result()
     return french["translations"][0]['translation']
 
-def frenchToEnglish(frenchText):
-    english = language_translator.translate(text=frenchText,model_id='fr-en').get_result()
+def french_to_english(french_text):
+    """
+    Translates a given French word or phrase to English
+    """
+    english = language_translator.translate(text=french_text,model_id='fr-en').get_result()
     return english["translations"][0]['translation']
-
-frenchToEnglish('')
